289a75734a5207ab8a09dc4a3320bfcca9627c0d
sonsodockje <hanaroo0115@naver.com>
2025-05-21 17:03:13 +0900

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,38 @@
 import React from 'react'
 import styles from './Footer.module.css'
-
+const URL_PREFIX = 'https://docs.github.com/en/site-policy'
+const footerItems = [
+	{
+		title: 'Terms',
+		link: `${URL_PREFIX}/github-terms/github-terms-of-service`,
+	},
+	{
+		title: 'Privacy',
+		link: `${URL_PREFIX}/github-terms/github-terms-of-service`,
+	},
+	{ title: 'Security', link: 'https://github.com/security' },
+	{ title: 'Status', link: 'https://www.githubstatus.com/' },
+	{ title: 'Docs', link: 'https://docs.github.com/en' },
+	{
+		title: 'Contact Github',
+		link: 'https://support.github.com/?tags=dotcom-footer',
+	},
+	{ title: 'Pricing', link: 'https://github.com/pricing' },
+	{ title: 'API', link: 'https://docs.github.com/en' },
+	{ title: 'Training', link: 'https://services.github.com/' },
+	{ title: 'Blog', link: 'https://github.blog/' },
+	{ title: 'About', link: 'https://github.com/about/' },
+]
 export default function Footer() {
-	return <div className={styles.footer}>푸터</div>
+	return (
+		<ul className={styles.footer}>
+			{footerItems.map((item, index) => (
+				<li key={index} className={styles.footerItem}>
+					<a href={item.link} className={styles.a}>
+						{item.title}
+					</a>
+				</li>
+			))}
+		</ul>
+	)
 }
