117d1f577d80aea97217856a1b8149e29e25b2ba
sonsodockje <hanaroo0115@naver.com>
2025-05-22 20:39:54 +0900

    open

--- src/components/ListContainer/ListContainer.jsx
+++ src/components/ListContainer/ListContainer.jsx
@@ -4,6 +4,8 @@ import Button from '../Button/Button.jsx'
 import ListItem from '../ListItem/ListItem.jsx'
 import ListItemLayout from '../ListItemLayout/ListItemLayout.jsx'
 
+import cx from 'clsx'
+
 export default function ListContainer() {
 	const [inputValue, setInputValue] = useState('is:pr is:open')
 
@@ -22,6 +24,7 @@ export default function ListContainer() {
 					style={{ backgroundColor: 'green', color: 'white' }}
 				/>
 			</div>
+			<OpenClosedFilters />
 			<ListItemLayout className={styles.listHeader}>
 				<div className={styles.filteringList}>
 					<span>Author</span>
@@ -46,5 +49,44 @@ export default function ListContainer() {
 				<ListItem title={'issue'} description={'디스트립션'} />
 			</div>
 		</div>
+	)
+}
+
+function OpenCloseFilter({ size, state, onClick, slected }) {
+	return (
+		<span
+			role="button"
+			onClick={onClick}
+			className={cx(styles.textFilter, {
+				[styles.sleted]: slected,
+			})}
+		>
+			{size} {state}
+		</span>
+	)
+}
+
+function OpenClosedFilters({ data }) {
+	const [isOpenMode, setIsOpenMode] = useState(true)
+
+	const openModeDataSize = 1
+	const closedModeDataSize = 2
+
+	return (
+		<>
+			<OpenCloseFilter
+				size={openModeDataSize}
+				state={'Open'}
+				slected={isOpenMode}
+				onClick={() => setIsOpenMode(true)}
+			/>
+
+			<OpenCloseFilter
+				size={closedModeDataSize}
+				state={'Closed'}
+				slected={!isOpenMode}
+				onClick={() => setIsOpenMode(false)}
+			/>
+		</>
 	)
 }
